8bf160ec641fb84f4125000c0930dbdf85bd6fbb
Ivan Sambol <117811964+Sambarone@users.noreply.github.com>
2023-09-18 19:36:09 +0200

    printing logic circuits with n inputs

--- sklop.c
+++ sklop.c
@@ -16,10 +16,20 @@ int main(int argc, char *argv[]) {
 	int a[kombinacija][ulaz];
 	int i,j;
 	int stupac=0;
-	int brojac=1;
+	int brojac=0;
+	int stup=0;
+	
+	
 	for (j=(ulaz-1);j>-1;j--){
+		stup++;
+		stupac=pow(2,stup);
+		printf("%d\n", stupac);
+		
 	for (i=0;i<kombinacija;i++){
-	a[i][j]=brojac;
+	if(brojac%stupac>=stupac/2)
+	a[i][j]=1;
+	else 
+	a[i][j]=0;
 		brojac++;
 		}
 		}
